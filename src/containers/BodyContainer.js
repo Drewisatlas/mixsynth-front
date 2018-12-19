c8cba197b489aa5e2f224066881fef7b8703f765
Drewisatlas <drewisatlas@gmail.com>
2018-12-19 16:31:54 -0500

    first commit

---
+++ src/containers/BodyContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BodyContainer extends React.Component {
+
+  render (){
+    return (
+      <div>
+        This is the Body
+      </div>
+    )
+  }
+}
+
+export default BodyContainer
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HeaderContainer extends React.Component {
+
+  render (){
+    return (
+      <div>
+        This is the header
+      </div>
+    )
+  }
+}
+
+export default HeaderContainer
